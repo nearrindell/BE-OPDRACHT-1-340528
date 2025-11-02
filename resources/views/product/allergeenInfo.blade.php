a6e5c2cfe8268a1d140354e6c11f935e8b48adaf
nearrindell <nathaniaarrindell3@gmail.com>
2025-11-02 22:22:13 +0100

    Allergeen Info pagina userstory gerealiseerd happy and unhappy userstory.

--- resources/views/product/allergeenInfo.blade.php
+++ resources/views/product/allergeenInfo.blade.php
@@ -1,1 +1,60 @@
-Hi ik ben de allergenen info page
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overzicht Allergenen</title>
+
+    @if($noAllergens)
+        <!-- redirect na 4 seconden naar product overzicht -->
+        <script>
+            setTimeout(function() {
+                window.location.href = "{{ route('product.index') }}";
+            }, 4000);
+        </script>
+    @endif
+</head>
+<body>
+    <div class="container d-flex justify-content-center">
+        <div class="col-md-8">
+            <br>
+            <h1>{{ $title }}</h1>
+            <p class="text-3xl">________________________________________</p>
+
+            <table class="table table-striped table-bordered align-middle shadow-sm">
+                <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Omschrijving</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if($noAllergens)
+                        <tr>
+                            <td colspan="2" class="text-center text-danger font-medium">
+                                In dit product zitten geen stoffen die een allergische reactie kunnen veroorzaken
+                            </td>
+                        </tr>
+                    @else
+                        @forelse ($allergenen as $allergeen)
+                            <tr>
+                                <td>{{ $allergeen->Naam }}</td>
+                                <td>{{ $allergeen->Omschrijving }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="2" class="text-center text-danger font-medium">
+                                    Geen allergenen beschikbaar voor dit product
+                                </td>
+                            </tr>
+                        @endforelse
+                    @endif
+                </tbody>
+            </table>
+
+            <a href="{{ route('product.index') }}" class="btn btn-secondary mt-3">Terug naar overzicht</a>
+        </div>
+    </div>
+</body>
+</html>
